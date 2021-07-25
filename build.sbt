785c9b0eb27375d9d79e82ae161ff83a804755cb
mohterbaord <mohterbaord@gmail.com>
2021-07-25 21:28:05 +0300

    zio-config for server

--- build.sbt
+++ build.sbt
@@ -1,12 +1,17 @@
 ThisBuild / scalaVersion := "2.13.6"
 ThisBuild / version := "0.1.0"
 
+val zioVersion = "1.0.9"
+val zioConfigVersion = "1.0.6"
+
 lazy val root = (project in file("."))
   .settings(
     name := "durak-right-version",
     libraryDependencies ++= Seq(
-      "dev.zio" %% "zio" % "1.0.9",
-      "dev.zio" %% "zio-test" % "1.0.9" % Test,
+      "dev.zio" %% "zio" % zioVersion,
+      "dev.zio" %% "zio-test" % zioVersion % Test,
+      "dev.zio" %% "zio-config" % zioConfigVersion,
+      "dev.zio" %% "zio-config-typesafe" % zioConfigVersion,
       "dev.zio" %% "zio-logging-slf4j" % "0.5.11",
       "io.d11" %% "zhttp" % "1.0.0.0-RC17"
     ),
--- src/main/scala/dev/derevnyadurakov/durakrightversion/DurakRightVersion.scala
+++ src/main/scala/dev/derevnyadurakov/durakrightversion/DurakRightVersion.scala
@@ -3,14 +3,51 @@ package dev.derevnyadurakov.durakrightversion
 import zhttp.http._
 import zhttp.service.server.ServerChannelFactory
 import zhttp.service.{EventLoopGroup, Server}
-import zio.logging.{LogFormat, LogLevel, Logging, log}
 import zio._
+import zio.config._
+import ConfigDescriptor._
+import zio.config.typesafe.TypesafeConfig
+import zio.logging.{LogFormat, LogLevel, Logging, log}
 
-import scala.util.Try
+import java.io.File
+import scala.util.{Success, Try}
 
 object DurakRightVersion extends App {
 
-  private val port = 8080
+  val defaultPort: Either[Int, String] = Left(8080)
+  val defaultServerConfig: ServerConfig = ServerConfig(defaultPort)
+  val defaultApplicationConfig: ApplicationConfig = ApplicationConfig(defaultServerConfig)
+
+  case class ServerConfig(port: Either[Int, String])
+
+  val serverConfigDescriptor: ConfigDescriptor[ServerConfig] =
+    int("port").orElseEither(string("port")).default(defaultPort)(
+      ServerConfig.apply,
+      ServerConfig.unapply
+    )
+
+  case class ApplicationConfig(server: ServerConfig)
+
+  val applicationConfigDescriptor: ConfigDescriptor[ApplicationConfig] =
+    nested("server")(serverConfigDescriptor).default(defaultServerConfig)(
+      ApplicationConfig.apply,
+      ApplicationConfig.unapply
+    )
+
+  val configs: TaskLayer[Has[ApplicationConfig]] = {
+    val path = getClass.getResource("/application.conf").getPath
+    TypesafeConfig.fromHoconFile(new File(path), applicationConfigDescriptor)
+  }
+
+  val port: RIO[Has[ApplicationConfig], Int] =
+    for {
+      config <- getConfig[ApplicationConfig]
+      tryPort = config.server.port match {
+        case Left(value) => Success(value)
+        case Right(value) => Try(value.toInt)
+      }
+      port <- ZIO.fromTry(tryPort)
+    } yield port
 
   private val app: UHttpApp = Http.collect {
     case Method.GET -> Root / "hello" =>
@@ -27,22 +64,30 @@ object DurakRightVersion extends App {
       } yield Response.text(dateTime.toString)
   }
 
-  private val server =
-    Server.port(port) ++
-      Server.app(app +++ routes)
-
   private val logger =
     Logging.console(
       logLevel = LogLevel.Info,
       format = LogFormat.ColoredLogFormat()
     ) >>> Logging.withRootLoggerName(DurakRightVersion.getClass.getCanonicalName)
 
+  private def server(port: Int) = Server.port(port) ++ Server.app(app +++ routes)
+
+  val infoServerStarted: RIO[Logging with Has[ApplicationConfig], Unit] =
+    for {
+      p <- port
+      _ <- log.info(s"Server started on port $p")
+    } yield ()
+
   override def run(args: List[String]): URIO[ZEnv, ExitCode] = {
     val nThreads = args.headOption.flatMap(x => Try(x.toInt).toOption).getOrElse(0)
-    server
-      .make
-      .use(_ => log.info(s"Server started on port $port") *> ZIO.never)
-      .provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto(nThreads) ++ logger)
+    (for {
+      p <- port
+      _ <- server(p)
+        .make
+        .use(_ => infoServerStarted *> ZIO.never)
+        .provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto(nThreads) ++ logger ++ configs)
+    } yield ())
+      .provideCustomLayer(configs)
       .exitCode
   }
 
